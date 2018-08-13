bcdb5d9c31adbc5f8f179de53c2e507bfa509d10
Jane Costa <yberdysh@gmail.com>
2018-08-12 20:46:04 -0400

    some changes'

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
 	const addButton = document.querySelector('#submit-btn')
 	console.log(addButton)
 	addButton.addEventListener("click", onSubmit)
+	fetchData()
 	// console.log(deleteButton)
 	// const choreList = document.getElementById('chore-list')
 
 
+function fetchData(){
 	fetch('http://localhost:3000/chores')
 	.then(res => res.json())
 	.then(data => {
@@ -37,39 +39,25 @@ document.addEventListener("DOMContentLoaded", () => {
 			})
 		})
 	})
+}
 
-	function onSubmit(){
+
+
+	function onSubmit(e){
 		console.log("SUBMIT")
 		fetch('http://localhost:3000/chores', {
 		method: 'POST',
-		// headers:{
-  //         Accept: 'applicaton/json',
-  //         'Content-Type': 'application/json'
-  //       },
+		headers:{
+          Accept: 'applicaton/json',
+          'Content-Type': 'application/json'
+        },
 		body: JSON.stringify({
 			title: document.querySelector('#title').value,
 			priority: document.querySelector('#priority').value,
 			duration: document.querySelector('#duration').value
 		})
 	})
-	// .then(res => res.json())
-	// .then(data => console.log(data))
-	// .then(data => {
-	// 	data.forEach(chore => {
-	// 		let choreDiv = document.createElement('div')
-	// 		choreDiv.innerHTML = `<button class="delete-button">X</button>
-	// 		<p>call method to delete</>
-	// 		<h3>${chore.title}</h3>
-	// 		<p>${chore.duration}</p>
-	// 		<p>${chore.priority}</p>`
-	// 		choreDiv.className = 'chore-card'
-	// 		choreDiv.setAttribute("data-id", chore.id)
-	// 		choreList.appendChild(choreDiv)
-
-	// 	})
-	// })
-
-	}
+}
 
 
 
